ff42ec0dece96701e50acce7ffb3796b3a7910da
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-09-30 15:22:23 -0400

    finished signup page and error messages

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -76,6 +76,6 @@ class TeachersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def teacher_params
-      params.require(:teacher).permit(:name, :email, :password)
+      params.require(:teacher).permit(:name, :email, :password, :password_confirmation)
     end
 end
