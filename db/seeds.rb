3176b3b927d465b3396c6c9ce95bec8b86defcc2
jwhaosis <jwhaosis@gmail.com>
2017-10-02 13:43:31 -0400

    database course additions

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ Course.delete_all
 Assignment.delete_all
 Submission.delete_all
 
-Course.create(course_id:12, course_title:"Advanced Programming Techniques")
-Course.create(course_id:21, course_title:"Data Structures and Algorithms")
-Course.create(course_id:166, course_title:"Capstone: Software Engineering")
+
+Course.create(course_code:12, course_title:"Advanced Programming Techniques")
+Course.create(course_code:21, course_title:"Data Structures and Algorithms")
+Course.create(course_code:166, course_title:"Capstone: Software Engineering")
