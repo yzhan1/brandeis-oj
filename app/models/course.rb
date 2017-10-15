cf8a6d783e75378093d473c255b82beb3c47239c
jwhaosis <jwhaosis@gmail.com>
2017-10-15 14:23:10 -0400

    through fix

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ApplicationRecord
   has_many :enrollments
   has_many :announcements
   has_many :assignments
+  has_many :submissions, :through => :assignments
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,5 +1,4 @@
 class Submission < ApplicationRecord
   belongs_to :assignment
   belongs_to :user
-  belongs_to :course, :through => :assignment
 end
