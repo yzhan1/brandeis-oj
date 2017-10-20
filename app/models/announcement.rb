cba2e92403a09fca1946a1c9d6a2d0050e7b4eb2
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-10-20 15:57:39 -0400

    basic breadcrumbs

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,3 +1,6 @@
 class Announcement < ApplicationRecord
+  validates :name, presence: true
+  validates :announcement_date, presence: true
+  validates :announcement_body, presence: true
   belongs_to :course
 end
--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,9 @@
 class Assignment < ApplicationRecord
+  validates :name, presence: true
+  validates :due_date, presence: true
+  validates :instructions, presence: true
+  validates :template, presence: true
+  validates :course_id, presence: true
   has_many :submissions
   belongs_to :course
 end
