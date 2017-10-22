d5a8300807ef9de301076c392490a91a5f86966a
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-10-21 21:01:55 -0400

    even more fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /^[a-zA-Z0-9_.+-]+@brandeis.edu/g
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
