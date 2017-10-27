dc76bc768a195c038a333bd2be6dc4d59b383c0d
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-10-27 16:21:56 -0400

    fixed heroku

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -5,10 +5,10 @@ class Submission < ApplicationRecord
   def run
     code = self.source_code
     dir_name = self.user.id
-    mkdir = `cd code\nmkdir #{dir_name}`
-    File.write("code/#{dir_name}/Solution.java", code)
-    res = `cd code\ncd #{dir_name}\njavac Solution.java\n java Solution`
-    remove = `rm -rf code/#{dir_name}`
+    mkdir = `cd tmp\nmkdir #{dir_name}`
+    File.write("tmp/#{dir_name}/Solution.java", code)
+    res = `cd tmp\ncd #{dir_name}\njavac Solution.java\n java Solution`
+    remove = `rm -rf tmp/#{dir_name}`
     return res
   end
 end
