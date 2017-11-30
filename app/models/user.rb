0d7a2f086783911eb8cbc1bf24572c1eb18fda12
Yaoming Zhan <zhanyaoming2014@gmail.com>
2017-11-30 16:21:52 -0500

    fixed stuffs

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password validations: false
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
   validates :role, presence: true
   has_many :submissions
   has_many :enrollments
