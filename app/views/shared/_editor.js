24ea350e4823cd53b331952f8d36bc319b783dec
jwhaosis <jwhaosis@gmail.com>
2017-12-01 16:47:17 -0500

    formatting for autosaving tabs

--- app/views/shared/_editor.js
+++ app/views/shared/_editor.js
@@ -1,11 +1,11 @@
 var editor = ace.edit("editor")
-var input = $('textarea[id="code_area"]').hide()
 editor.$blockScrolling = Infinity
 editor.setTheme("ace/theme/monokai")
 editor.getSession().setUseSoftTabs(false)
 editor.getSession().setMode("ace/mode/java")
 editor.setShowPrintMargin(false)
 editor.setOptions({fontSize: "14px"})
+
 var temp_id = 1
 
 $(".nav-tabs").on("click", "a", function(e) {
@@ -35,7 +35,6 @@ $('.add-class').click(function(e) {
   $(this).closest('li').before(`<li class="nav-item"><a id="tab_${ tabId }" class="nav-link active" href="#${ tabId }" data-toggle="tab">${tabName}</a> <span> x </span></li>`)
   $('.tab-content').append(`<div class="tab-pane active" id="${ tabId }"><pre id="editor_${ id }"  style="margin-bottom: 0px" class="editor-div"></pre></div>`)
   var editor = ace.edit("editor_"+id)
-  var input = $('textarea[id="submission_source_code"]').hide()
   editor.$blockScrolling = Infinity
   editor.setTheme("ace/theme/monokai")
   editor.getSession().setUseSoftTabs(false)
