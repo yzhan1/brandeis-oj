f8440cc3580d401154ff43488bceb93ed8ebb199
Shu Lin Chan <shulin.chan@gmail.com>
2017-12-11 16:25:51 -0500

    Fix rebase error

--- app/views/shared/_editor.js
+++ app/views/shared/_editor.js
@@ -5,14 +5,13 @@ editor.getSession().setUseSoftTabs(false)
 editor.getSession().setMode("ace/mode/java")
 editor.setShowPrintMargin(false)
 editor.setOptions({fontSize: "14px"})
-<<<<<<< HEAD
 var temp_id = 20
 
 $(".nav-tabs").on("click", "a", function(e) {
   e.preventDefault()
   if (!$(this).hasClass('add-class')) {
-    $('a').removeClass('active');
-    $(this).addClass('active');
+    $('a').removeClass('active')
+    $(this).addClass('active')
   }
 }).on("click", "span", function() {
   var anchor = $(this).siblings('a')
@@ -20,7 +19,7 @@ $(".nav-tabs").on("click", "a", function(e) {
   $(this).parent().remove()
   console.log()
   $('.nav-tabs li').children('a').first().click()
-  $(this).addClass('active');
+  $(this).addClass('active')
 })
 
 $('.add-class').click(function(e) {
@@ -34,7 +33,7 @@ $('.add-class').click(function(e) {
 
   $('.nav-tabs').append(`<li class="nav-item"><a id="tab_${ tabId }" class="nav-link active" href="#${ tabId }" data-toggle="tab">${tabName}</a> <span> x </span></li>`)
   $('.tab-content').append(`<div class="tab-pane active" id="${ tabId }"><pre id="editor_${ id }"  style="margin-bottom: 0px" class="editor-div"></pre></div>`)
-  var editor = ace.edit("editor_"+id)
+  var editor = ace.edit("editor_" + id)
   editor.$blockScrolling = Infinity
   editor.setTheme("ace/theme/monokai")
   editor.getSession().setUseSoftTabs(false)
@@ -42,9 +41,9 @@ $('.add-class').click(function(e) {
   editor.setShowPrintMargin(false)
   editor.setOptions({fontSize: "14px"})
   $('.tab-content').append(editor)
-  document.getElementById("tab_"+tabId).click()
+  document.getElementById("tab_" + tabId).click()
   document.getElementById('file_name_input').value=''
-  $('#create_tab_button').attr('disabled',true);
+  $('#create_tab_button').attr('disabled', true)
   $.ajax({
     type: 'POST',
     url: '/new_code',
@@ -54,7 +53,7 @@ $('.add-class').click(function(e) {
 
 $('.delete-file').click(function(e) {
   e.preventDefault()
-  var fileName = $(this).attr("value");
+  var fileName = $(this).attr("value")
   var submissionId = document.getElementById('submission_id').value
   $.ajax({
     type: 'POST',
@@ -62,5 +61,5 @@ $('.delete-file').click(function(e) {
     data: {filename: fileName, submission_id: submissionId}
   })
   //Delete from page
-  $(this).parent('.row').remove();
+  $(this).parent('.row').remove()
 })
