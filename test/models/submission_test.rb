dfaf9a15287236f1f27c117e2f2bdf2d93254b53
Shu Lin Chan <shulin.chan@gmail.com>
2017-12-17 23:07:20 -0500

    Some model tests

--- test/models/submission_test.rb
+++ test/models/submission_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class SubmissionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @assignment = assignments(:one)
+    @submission = submissions(:submission_one)
+  end
+
+  test 'assignment valid' do
+    assert @assignment.valid?
+  end
+
+  test 'submission valid' do
+    assert @submission.valid?
+  end
+
+  test 'submission without user is invalid' do
+    submission = Submission.new(source_code: 'Test code')
+    refute submission.valid?
+    assert_not_nil submission.errors[:user]
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,5 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
+
+  setup do
+    @user = users(:test_student)
+    @user_two = users(:another_student)
+    @test_teacher = users(:test_teacher)
+    @course = courses(:course_one)
+    @course_two = courses(:course_two)
+    @test_teacher.enroll_course('11a')
+  end
+
+  test 'valid student' do
+    assert @user.valid?
+  end
+
+  test 'valid teacher' do
+    assert @test_teacher.valid?
+  end
+
+  test 'invalid without email' do
+    user = User.new(name: 'John')
+    refute user.valid?
+    assert_not_nil user.errors[:email]
+  end
 end
